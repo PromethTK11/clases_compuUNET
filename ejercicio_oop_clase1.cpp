d17e201dd721837c21180223f186d9dd8d5b449d
PromethTK11 <linoruben@hotmail.com>
2018-01-30 20:22:42 -0400

    Update from this shit, final version

--- ejercicio_oop_clase1.cpp
+++ ejercicio_oop_clase1.cpp
@@ -4,7 +4,8 @@
 //Para N estudiantes
 
 #include <iostream>
-#include <conio.h>
+#include <string>
+
 using namespace std;
 
 class ESTUDIANTE
@@ -15,18 +16,62 @@ class ESTUDIANTE
 		void listar(); //Mostrar los datos
 	
 	private:
-		char nom[12];
+		string nom;
 		int notas[3]; //Validar 1 - 9
-		float prom	
+		float prom;
 };
 
 void ESTUDIANTE::cargar()
 {
 	cout << "Ingrese nombre del estudiante: " << endl;
-	cin >> nombre;
+	cin.ignore(256, '\n');	//para limpiar el \n en enter, como un fflush
+	getline(cin, nom);	//se usa getline en vez de cout para mantener espacios en nombres
 	for (int i=0;i<3;i++)
 	{
-		cout << "Ingrese nota numero " << i+1 << endl;
-		cin << notas[i];		
+		do {
+		cout << "Ingrese nota numero " << i+1 << " del 1 al 9" << endl;
+		cin >> notas[i];
+		} while (notas[i]<1 || notas[i]>9);
 	}
 }
+
+void ESTUDIANTE::promedio()
+{
+	prom = 0;
+	for (int i=0;i<3;i++)
+	{
+		prom += notas[i];
+	}
+	prom /= 3;
+}
+
+void ESTUDIANTE::listar()
+{
+	cout << nom << "\t" << notas[0] << "\t" << notas[1] << "\t" << notas[2] 
+		<< "\t" << prom << endl;
+}
+
+int main(){
+	ESTUDIANTE classroom[20];
+	int n;
+	cout << "Booting up... \nBienvenido\n" << endl;
+		do {		
+		cout << "Ingrese la cantidad de estudiantes que desee procesar (Del 1 al 20)" << endl;
+		cin >> n;
+		} while (n<1 || n>20);
+	for (int k=0;k<n;k++)
+	{
+		classroom[k].cargar();
+		classroom[k].promedio();
+	}	
+	cout << "Nombre\t\tNota 1\tNota 2\tNota 3\tPromedio" << endl;
+	cout << "================================================" << endl;
+	for (int k=0;k<n;k++)
+	{
+		classroom[k].listar();
+	}	
+	cout << "Cerrando programa..." << endl;
+return 0;	
+}
+
+
